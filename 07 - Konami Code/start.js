93dfabb7442abdd04129de38c7baf73248ed7294
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-06-02 23:28:01 +0700

    [Added] Konami Code

---
+++ 07 - Konami Code/start.js
@@ -0,0 +1,33 @@
+(() => {
+  // เริ่มเขียนโค้ด
+  const konamiCode = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+  ];
+
+  let index = 0;
+
+  function onKeyDown(event) {
+    event.key === konamiCode[index] ? index++ : (index = 0);
+
+    console.log(event.key);
+
+    if (konamiCode.length === index) {
+      startSnowing();
+    }
+  }
+
+  function run() {
+    document.addEventListener("keydown", onKeyDown);
+  }
+
+  run();
+})();
