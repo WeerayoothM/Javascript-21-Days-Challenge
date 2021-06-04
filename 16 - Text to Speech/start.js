f08581e45843eb919b5dab64578bc5c65d98caa3
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-06-04 22:43:35 +0700

    [Added] Text to Speech

---
+++ 16 - Text to Speech/start.js
@@ -0,0 +1,23 @@
+(() => {
+  // เริ่มเขียนโค้ด
+  const message = new SpeechSynthesisUtterance();
+
+  function onVoiceChanged() {
+    const voices = speechSynthesis.getVoices();
+    const thVoice = voices.find((voice) => voice.lang === "th-TH");
+    message.voice = thVoice;
+  }
+
+  function onClick(event) {
+    message.text = event.target.getAttribute("alt");
+    speechSynthesis.speak(message);
+  }
+  function run() {
+    speechSynthesis.addEventListener("voiceschanged", onVoiceChanged);
+
+    const imgElems = Array.from(document.querySelectorAll("img"));
+    imgElems.forEach((imgElem) => imgElem.addEventListener("click", onClick));
+  }
+
+  run();
+})();
